40030155741845d27201cc0f16508143aaf96447
Ibrahim Eid <ie1239@fayoum.edu.eg>
2026-06-23 12:49:49 +0300

    add hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NotPadAiModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotPadAiModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NotPadAiModel({
